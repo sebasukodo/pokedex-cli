77e13fdf587149ec76e7ce3a7932126328dd3bb7
sebasukodo <sebastianlein@web.de>
2025-09-30 09:02:45 +0200

    introduce repl logic

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		if strings.ToLower(input) == "q" || strings.ToLower(input) == "quit" {
+			break
+		}
+
+		words := strings.Fields(strings.ToLower(input))
+		fmt.Println("Your command was:", words[0])
+
+	}
 }
 
 func cleanInput(text string) []string {
